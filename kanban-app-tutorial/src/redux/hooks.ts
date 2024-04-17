0a14b2e8103862177e1e075d71324240dea32b39
RichardMiruka <richardonsare15@gmail.com>
2024-04-17 15:27:18 +0300

    creating typed versions of the useDispatch and useSelector hooks to ensure type safety when interacting with the Redux store

---
+++ kanban-app-tutorial/src/redux/hooks.ts
@@ -0,0 +1,7 @@
+// hooks.ts
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import type { RootState, AppDispatch } from "./store";
+// Typed versions of useDispatch and useSelector hooks
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
