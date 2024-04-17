7bd3379827aa214ab08b8e8c371553011b032385
RichardMiruka <richardonsare15@gmail.com>
2024-04-17 14:37:17 +0300

    imported the options variable from the option.ts file

--- kanban-app-tutorial/src/app/api/auth/[...nextauth]/route.ts
+++ kanban-app-tutorial/src/app/api/auth/[...nextauth]/route.ts
@@ -3,4 +3,6 @@ import { options } from "./options";
 
 const handler = NextAuth(options);
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }; 
+//ensures that any GET or POST request sent to the 
+// api/auth/[...nextauth]route will be managed by next-auth.js
